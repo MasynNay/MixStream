c84c383f8487a61e77933fe2234ddc4c1dcc264c
unknown <charliegarvin90@gmail.com>
2023-01-26 21:31:52 -0700

    added in javascript for imdb results

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ const options = {
         'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
     }
 };
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
 var textInput = document.querySelector('#textInput')
 var searchBtn = document.querySelector('#search-button')
 var search
@@ -14,18 +18,22 @@ function getUtelly() {
     var utellyURL = `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${search}&country=us`
     return utellyURL
 }
+function getTitle() {
+    var imdbUrl = `https://imdb-api.com/en/API/Title/k_gxh3r11l/${imdbCode}/FullCast,Posters,Ratings`
+    return imdbUrl;
+}
 
 function searchOption(event) {
     event.preventDefault()
     search = textInput.value
-    console.log(search);
+    // console.log(search);
 
     async function utellyInfo() {
         const response = await fetch(getUtelly(), options)
         const data = await response.json()
-        console.log(data);
+        // console.log(data);
         imdbCode = data.results[0].external_ids.imdb.id
-        console.log(imdbCode);
+        // console.log(imdbCode);
 
         var results = data.results[0]
         var favoriteEl = document.querySelector('.favorites')
@@ -40,7 +48,24 @@ function searchOption(event) {
             <img class="marketing-site-content-section" src="${movie.icon}" alt="${movie.display_name} icon">
             </a>`
         }).join(' ');
+
+        const imdbResults = await fetch(getTitle(), requestOptions)
+        const imdbData = await imdbResults.json()
+        console.log(imdbData);
+        var posterEl = document.querySelector('#imdb-poster')
+         posterEl.innerHTML = `<img alt="media poster" src="${imdbData.image}">`
+        var ratingsEl = document.querySelector('#imdb-ratings')
+         ratingsEl.innerHTML = `<p>IMDB Rating: ${imdbData.imDbRating}</p>`
+        var castEl = document.querySelector('#imdb-cast')
+         castEl.innerHTML = imdbData.fullCast.actors.map((actor, idx) => {
+            if (idx <= 2) {
+                return `<li>${actor.name} as ${actor.asCharacter}</li>`
+            }
+         }).join('')
+         
     }
+
+   
 
     return utellyInfo()
 }
@@ -50,3 +75,28 @@ function searchOption(event) {
 
 
 searchBtn.addEventListener("click", searchOption)
+
+
+   
+var searchBtn = document.querySelector('#search-btn')
+var search = document.textInput;
+
+
+  function getImdb () {
+    var imdbUrl = `https://imdb-api.com/en/Search/k_gxh3r11l`
+    return imdbUrl;
+};
+
+
+function imdbResponse () {
+
+}
+
+
+//searchBtn.addEventListener('click', getImdb());
+
+  
+// TODO: set movies into local storage
+// TODO: call local storage to display to grid
+// TODO: add event listener for favorite button
+// TODO: add event listener for favorite results
